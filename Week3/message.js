384cc023c07a9998ab72bad414c3ea9ab0b0f249
Mairajali11 <49980275+Mairajali11@users.noreply.github.com>
2024-08-29 17:11:58 +0500

    Week 3 class 2

---
+++ Week3/message.js
@@ -0,0 +1,12 @@
+console.log("Hello Wolrd!");
+var a=10;let b=5;
+c=a+b;
+console.log(c)
+c1=a*b*100;
+console.log(c1)
+if (c>100){
+console.log("c is greater than 100")}
+else {
+console.log("C is less than 100")}
+var a=5;
+console.log(a)
