4f570c90a113442281bcfc5235d02673feacb8a1
Mairajali11 <49980275+Mairajali11@users.noreply.github.com>
2024-08-30 17:13:47 +0500

    Week 3 Class 3

--- Week3/message.js
+++ Week3/message.js
@@ -1,6 +1,6 @@
 console.log("Hello Wolrd!");
 var a=10;let b=5;
-c=a+b;
+c=a/b;
 console.log(c)
 c1=a*b*100;
 console.log(c1)
@@ -10,3 +10,15 @@ else {
 console.log("C is less than 100")}
 var a=5;
 console.log(a)
+var marks;
+marks=20;
+console.log(marks)
+var pi =3;
+console.log("value of pi 1 is "+ pi++)
+console.log("value of pi 1a is "+ pi)
+console.log("value of pi 2 is "+ pi--)
+console.log("value of pi 2a is "+ pi)
+console.log("value of pi 3 is "+ pi+5)
+console.log("value of pi 4 is "+ pi)
+console.log("value of pi 5 is "+ ++pi)
+console.log("value of pi 6 is "+ pi)
