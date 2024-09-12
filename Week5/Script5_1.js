72d0dd158eebee567ae5ddfb2c28836c2402c5f5
Mairajali11 <49980275+Mairajali11@users.noreply.github.com>
2024-09-11 20:41:12 -0700

    week5_Class2

---
+++ Week5/Script5_1.js
@@ -0,0 +1,6 @@
+let fruits = ["Mango", "Banana", "Orange", "Grape", "Apple", "Pear", "Cherry", "Strawberry"]; // Initializing Fruits
+console.log(fruits);
+// Loop through the array and log each element
+for (let i = 0; i < fruits.length; i++) {
+    console.log(`Fruit at index ${i}: ${fruits[i]}`);
+}
